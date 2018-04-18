c699f0e958586ea4e0d541170cab0f3d04f80e6f
hrarnob07 <hrarnob07@gmail.com>
2018-04-19 00:31:01 +0600

    database table

---
+++ database/migrations/2018_01_24_143045_create_likes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('post_id');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+  
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
